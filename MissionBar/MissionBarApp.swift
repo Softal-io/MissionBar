8fbbf7443f523bd294313783daf0a054c718330a
Ram Patra <ramswarooppatra@gmail.com>
2025-07-29 16:41:00 +0100

    Initial Commit

---
+++ MissionBar/MissionBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MissionBarApp.swift
+//  MissionBar
+//
+//  Created by Ram Patra on 29/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct MissionBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
